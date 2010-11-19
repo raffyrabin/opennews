e0ef112480ea65d05547626c59af0a9535c95f55
Raffy Rabin <raffy@raffyrabin.com>
2010-11-18 20:17:43 -0500

    Project skelt on

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opennews2::Application.config.secret_token = 'f6c228fab1cabaef36bfe803843cf476a363eccbaa675bd09497d5ddd6de90439a3ae96e5943c86e3d5ca17ac255a9a72452fa19c328e0b185f6c83f3897adcb'
